24dd6cca1096e33119b976353475c4732b11d110
Hasni Fodeilla <Hasni-fodeilla99@hotmail.com>
2020-03-03 16:44:41 +0100

    first commit

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,33 @@
 var express = require('express');
 var router = express.Router();
-var mongoose = require('mongoose');
+var uid2 = require("uid2");
+var SHA256 = require("crypto-js/sha256");
+var encBase64 = require("crypto-js/enc-base64");
 
 var userModel = require('../models/users');
 var agenceModel = require('../models/agences');
 var annonceModel = require('../models/annonces');
 var rdvModel = require('../models/rdv');
 
+var salt = uid2(15)
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+/* ROUTES SingIn SignUp Hasni */
+
+router.post("/SingUp", async function(req, res,next){
+  var newUser = new userModel ({
+    nom: req.body.nom, 
+    prenom: req.body.prenom,
+    email: req.body.email,
+    password: SHA256(req.body.mdp + salt).toString(encBase64),
+    token : uid2(15),
+    salt :salt
+  })
+  await newUser.save();
+ res.json({sucess:true,newUser})
+})
 
 module.exports = router;
